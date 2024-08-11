d0dce025cb1650796a1ca21fc1c29368cd01235e
Murali981 <muraliju981@gmail.com>
2024-08-11 14:09:06 +0530

    Completed the data fetching feature with react router using useLoaderData custom hook provided by react-router-dom

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData(); // This is the custom hook provided by the react-router-dom , So it automatically loads the data from the
+  // loader which we provided in the "/menu" route
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu(); // This is an API request in the apiRestaurant.js file
+  return menu;
 }
 
 export default Menu;
